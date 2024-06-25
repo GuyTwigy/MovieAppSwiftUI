e5d2dc3796ed610f103dd04f70809387bf913613
Guy Twig <twigy77@gmail.com>
2024-06-25 12:24:18 +0300

    Initial Commit

---
+++ MovieAppSwiftUI/MovieAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppSwiftUIApp.swift
+//  MovieAppSwiftUI
+//
+//  Created by Guy Twig on 25/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
