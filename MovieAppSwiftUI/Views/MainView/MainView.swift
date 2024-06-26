d27b5cf0263e5a4f4a30fae9e9eabb19814b034f
Guy Twig <twigy77@gmail.com>
2024-06-26 14:18:57 +0300

    resign first responder when scroll is scrolling

--- MovieAppSwiftUI/Views/MainView/MainView.swift
+++ MovieAppSwiftUI/Views/MainView/MainView.swift
@@ -15,13 +15,6 @@ struct MainView: View {
     @State private var optionTitle = "Top Rated"
     
     private let optionsArr: [String] = ["Top Rated", "Popular", "Trending", "Now Playing", "Upcoming"]
-    private let movieData: [MovieData] = [
-        MovieData(id: 1, idString: "1", title: "title 1", posterPath: "posterPath 1", overview: "overview 1", releaseDate: "releaseDate 1", originalLanguage: "originalLanguage 1", voteAverage: 10.0, date: Date()),
-        MovieData(id: 2, idString: "2", title: "title 2", posterPath: "posterPath 2", overview: "overview 2", releaseDate: "releaseDate 2", originalLanguage: "originalLanguage 2", voteAverage: 9.0, date: Date()),
-        MovieData(id: 3, idString: "3", title: "title 3", posterPath: "posterPath 3", overview: "overview 3", releaseDate: "releaseDate 3", originalLanguage: "originalLanguage 3", voteAverage: 8.0, date: Date()),
-        MovieData(id: 4, idString: "4", title: "title 4", posterPath: "posterPath 4", overview: "overview 4", releaseDate: "releaseDate 4", originalLanguage: "originalLanguage 4", voteAverage: 7.0, date: Date()),
-        MovieData(id: 5, idString: "5", title: "title 5", posterPath: "posterPath 5", overview: "overview 5", releaseDate: "releaseDate 5", originalLanguage: "originalLanguage 5", voteAverage: 6.0, date: Date()),
-        MovieData(id: 6, idString: "6", title: "title 6", posterPath: "posterPath 6", overview: "overview 6", releaseDate: "releaseDate 6", originalLanguage: "originalLanguage 6", voteAverage: 5.0, date: Date())]
     
     var body: some View {
         NavigationView {
@@ -120,7 +113,7 @@ struct MainView: View {
                                     }
                                 }
                             NavigationLink(destination: MovieDetailsView(vm: MovieDetailsViewModel(dataService: vm.dataService, movie: movie))) {
-                                EmptyView() // Use EmptyView to remove the arrow
+                                EmptyView()
                             }
                             .opacity(0)
                         }
@@ -129,7 +122,10 @@ struct MainView: View {
                         .listRowSeparator(.hidden)
                     }
                     .listStyle(PlainListStyle())
-                    .background(Color(.systemGray2)) // Set background color here
+                    .background(Color(.systemGray2))
+                    .gesture(DragGesture().onChanged { _ in
+                                            UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                                        })
                     .onChange(of: selectedOption) { _ in
                         withAnimation {
                             proxy.scrollTo(vm.moviesList.first?.id, anchor: .top)
