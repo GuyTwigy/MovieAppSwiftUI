0152931c2974b0bc74640159996ff2a52612edb9
Guy Twig <twigy77@gmail.com>
2024-06-27 18:13:16 +0300

    rearange presentation, make code redable

--- MovieAppSwiftUI/Views/MovieDetailsView/MovieDetailsView.swift
+++ MovieAppSwiftUI/Views/MovieDetailsView/MovieDetailsView.swift
@@ -18,91 +18,21 @@ struct MovieDetailsView: View {
     @State private var isShareSheetPresented = false
     
     var body: some View {
-        Text(vm.movie?.title ?? "")
-            .multilineTextAlignment(.center)
-            .font(.system(size: 30, weight: .bold))
-            .frame(maxWidth: .infinity, alignment: .center)
-            .padding(.horizontal)
-            .padding(.top, 10)
+        
+        movieTitle()
         
         ScrollView {
             VStack(alignment: .leading, spacing: 10) {
                 if !vm.showUIError {
-                    ZStack(alignment: .topTrailing) {
-                        ProgressView()
-                            .progressViewStyle(CircularProgressViewStyle())
-                        
-                        if let imageURL = Utils.getImageUrl(posterPath: vm.movie?.posterPath ?? "") {
-                            KFImage(imageURL)
-                                .resizable()
-                                .frame(minWidth: UIScreen.main.bounds.width)
-                                .aspectRatio(contentMode: .fill)
-                        }
-                        
-                        Button(action: {
-                            isShareSheetPresented.toggle()
-                        }) {
-                            Image(systemName: "square.and.arrow.up")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 30, height: 30)
-                                .padding(10)
-                                .background(Color.white.opacity(0.7))
-                                .cornerRadius(15)
-                                .foregroundStyle(ForegroundStyle())
-                        }
-                        .sheet(isPresented: $isShareSheetPresented) {
-                            if let movie = vm.movie {
-                                ShareSheetView(movie: movie)
-                            }
-                        }
-                    }
-                    .padding()
                     
-                    VStack(alignment: .leading, spacing: 10) {
-                        ForEach(vm.detailsArr, id: \.self) { detail in
-                            SingleDetailView(title: detail.title, subtitle: detail.description)
-                                .padding(.horizontal)
-                                .background(Color(UIColor.systemBackground))
-                        }
-                    }
+                    imageAndShareBtn()
                     
-                    ZStack {
-                        HStack {
-                            Button(action: {
-                                print("show trailer tapped")
-                                Task {
-                                    await vm.getTrailer(id: vm.movie?.id ?? 1)
-                                    if vm.video != nil {
-                                        showTrailerSheet = true
-                                    }
-                                    if vm.showTrailerError {
-                                        showError = true
-                                        errorMessage = "Failed to load trailer"
-                                        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-                                            withAnimation {
-                                                showError = false
-                                                errorMessage = ""
-                                            }
-                                        }
-                                    }
-                                }
-                            }) {
-                                Text("Show Trailer")
-                                    .frame(maxWidth: .infinity)
-                                    .frame(height: 50)
-                                    .foregroundColor(Color.white)
-                                    .background(Color.blue)
-                                    .cornerRadius(10)
-                                    .padding()
-                            }
-                            .padding(.horizontal, 10)
-                        }
-                    }
+                    singleDetailViewRows()
+                    
+                    trailerBtn()
+                    
                 } else {
-                    ErrorView(showError: $vm.showUIError, errorMessage: "Movie is not available")
-                        .foregroundColor(.black)
-                        .padding()
+                    errorView()
                 }
             }
             .background(Color(UIColor.systemBackground))
@@ -131,6 +61,99 @@ struct MovieDetailsView: View {
                 }
             }
         )
+    }
+    
+    func movieTitle() -> some View {
+        Text(vm.movie?.title ?? "")
+            .multilineTextAlignment(.center)
+            .font(.system(size: 30, weight: .bold))
+            .frame(maxWidth: .infinity, alignment: .center)
+            .padding(.horizontal)
+            .padding(.top, 10)
+    }
+    
+    func imageAndShareBtn() -> some View {
+        ZStack(alignment: .topTrailing) {
+            ProgressView()
+                .progressViewStyle(CircularProgressViewStyle())
+            
+            if let imageURL = Utils.getImageUrl(posterPath: vm.movie?.posterPath ?? "") {
+                KFImage(imageURL)
+                    .resizable()
+                    .frame(minWidth: UIScreen.main.bounds.width)
+                    .aspectRatio(contentMode: .fill)
+            }
+            
+            Button(action: {
+                isShareSheetPresented.toggle()
+            }) {
+                Image(systemName: "square.and.arrow.up")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 30, height: 30)
+                    .padding(10)
+                    .background(Color.white.opacity(0.7))
+                    .cornerRadius(15)
+                    .foregroundStyle(ForegroundStyle())
+            }
+            .sheet(isPresented: $isShareSheetPresented) {
+                if let movie = vm.movie {
+                    ShareSheetView(movie: movie)
+                }
+            }
+        }
+        .padding()
+    }
+    
+    func singleDetailViewRows() -> some View {
+        VStack(alignment: .leading, spacing: 10) {
+            ForEach(vm.detailsArr, id: \.self) { detail in
+                SingleDetailView(title: detail.title, subtitle: detail.description)
+                    .padding(.horizontal)
+                    .background(Color(UIColor.systemBackground))
+            }
+        }
+    }
+    
+    func trailerBtn() -> some View {
+        ZStack {
+            HStack {
+                Button(action: {
+                    print("show trailer tapped")
+                    Task {
+                        await vm.getTrailer(id: vm.movie?.id ?? 1)
+                        if vm.video != nil {
+                            showTrailerSheet = true
+                        }
+                        if vm.showTrailerError {
+                            showError = true
+                            errorMessage = "Failed to load trailer"
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                                withAnimation {
+                                    showError = false
+                                    errorMessage = ""
+                                }
+                            }
+                        }
+                    }
+                }) {
+                    Text("Show Trailer")
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 50)
+                        .foregroundColor(Color.white)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                        .padding()
+                }
+                .padding(.horizontal, 10)
+            }
+        }
+    }
+    
+    func errorView() -> some View {
+        ErrorView(showError: $vm.showUIError, errorMessage: "Movie is not available")
+            .foregroundColor(.black)
+            .padding()
     }
 }
 
